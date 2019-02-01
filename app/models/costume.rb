26873b46d8cca370249da0825743f3a0db140c01
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-01 17:00:57 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,6 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
-class Costume ActiveRecord::Migration[4.2]
+class Costume ActiveRecord::Migration
   def change
   end
 end
