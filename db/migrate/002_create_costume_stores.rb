3490c87a61c7fd714c6e0db40c7093c452a52c26
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-01 17:12:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,13 @@
 # Create your costume_stores migration here
-def  
+class CreateCostumesStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumestore do |t|
+      t.string :name
+      t.string :location
+      t.integer :costumes
+      t.string :image_url
+
+      t.timestamps null: false
+    end
+  end
+end
