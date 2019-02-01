60023992e0e5140d931f913e8363eba3c165e1a2
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-01 17:23:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,18 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -21,20 +33,15 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "costumestore", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costumes"
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
+    t.string "location"
     t.float "price"
-    t.string "size"
-    t.string "image_url"
+    t.string "theme"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
