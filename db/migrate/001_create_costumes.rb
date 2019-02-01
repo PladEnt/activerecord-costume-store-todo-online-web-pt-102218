32e65a6c921c6297f25ba13f64220636f52e8120
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-01 17:07:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,9 +11,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.float :price
       t.string :size
       t.string :image_url
-      t.float :price
 
       t.timestamps null: false
     end
